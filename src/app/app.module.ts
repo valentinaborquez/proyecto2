4c825c2cc78bc5697e81c7ea4ccc90378b684b8b
peachyvvalentine <55882490+peachyvvalentine@users.noreply.github.com>
2019-12-10 09:29:55 -0300

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ListalumnosComponent } from './components/listalumnos/listalumnos.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { ProfesorComponent } from './components/profesor/profesor.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'listalumnos', component: ListalumnosComponent }
+  { path: 'inicio', component: InicioComponent },
+  { path: 'listalumnos', component: ListalumnosComponent },
+  { path: 'profesor', component: ProfesorComponent },
+  { path: 'registro', component: RegistroComponent }
 ];
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ListalumnosComponent
+    ListalumnosComponent,
+    InicioComponent,
+    RegistroComponent,
+    ProfesorComponent
   ],
   imports: [
     RouterModule.forRoot(
