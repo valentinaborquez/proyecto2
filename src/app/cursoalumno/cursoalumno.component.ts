f562071d6ac03d6e0bafd4fea3e5cc4328f49831
valentinaborquez <peachyvvalentine@gmail.com>
2019-12-15 21:15:11 -0400

    ult act

---
+++ src/app/cursoalumno/cursoalumno.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cursoalumno',
+  templateUrl: './cursoalumno.component.html',
+  styleUrls: ['./cursoalumno.component.css']
+})
+export class CursoalumnoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
